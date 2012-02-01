1b3840aee020b6acb93f96ded3a44b060589bcc9
Tim Dennis <dennis.tim@gmail.com>
2012-01-31 16:35:42 -0800

    info for variables in comments

--- ogpxml.py
+++ ogpxml.py
@@ -15,15 +15,17 @@ sheet = sheetnames[0]
 
 for row in xl.getiter(sheet):
   print row
-
+  write_xmlfile(write_to_xml(row))
+  
 
 def write_to_xml(xls_row_as_dict):
     '''takes dict representing a row in a geo xls and creates a xml str'''
+	#need to persist the layerID & the callnumber+filename in sqlite db
     xml = xmlwitch.Builder()
     with xml.add(allowDups='false'):
         with xml.doc():
             xml.field("450281", name='LayerId') #need to retrieve id & increment
-            xml.field("schooldistricts", name='Name')#shld be call_number & file name from xls
+            xml.field(xls_row_as_dict.CALLNUMBER+xls_row_as_dict.FILENAME, name='Name')#shld be call_number & file name from xls
             xml.field('UCB', name='WorkspaceName')
             xml.field('initial collection', name='CollectionId')
             xml.field('Berkeley', name='Institution')
@@ -32,24 +34,28 @@ def write_to_xml(xls_row_as_dict):
             xml.field('Polygon', name='DataType')
             xml.field('Polygon', name='DataTypeSort')
             xml.field('Online', name='Availability')
-            xml.field('Napa County School District boundaries', name='LayerDisplayName')#?? where from xls
-            xml.field('Napa County School District boundaries', name='LayerDisplayNameSort')
+            xml.field('Napa County School District boundaries', name='LayerDisplayName')# date=year,  john will add column for location & theme 
+            xml.field('Napa County School District boundaries', name='LayerDisplayNameSort') # same as above
             xml.field('UC Berkeley Libraries', name='Publisher')
             xml.field('UC Berkeley Libraries', name='PublisherSort')
             xml.field("County of Napa Assessor's Office", name='Originator')
             xml.field("County of Napa Assessor's Office", name='OriginatorSort')
             xml.field('society boundaries Schools School District Education', name='ThemeKeywords')
-            xml.field('American Canyon Napa St. Helena Yountville Calistoga Napa County California', name='PlaceKeywords')
+            xml.field('American Canyon Napa St. Helena Yountville Calistoga Napa County California', name='PlaceKeywords') # = sheetname in xls
             xml.field('Boundaries for school districts in Napa County.', name='Abstract' )
             xml.field('http://gis-gs.lib.berkeley.edu:8080/geoserver/gwc/service/wms', name='Location')
-            xml.field('38.864947', name='MaxY')
-            xml.field('38.153318', name='MinY')
-            xml.field('-122.649217', name='MinX')
-            xml.field('-122.061226', name='MaxX')
+            xml.field('38.864947', name='MaxY') # in xls
+            xml.field('38.153318', name='MinY') # in xls
+            xml.field('-122.649217', name='MinX') # in xls
+            xml.field('-122.061226', name='MaxX') # in xls
             xml.field('-122.3552215', name='CenterX')
             xml.field('38.5091325', name='CenterY')
             xml.field('0.2939954999999941', name='HalfWidth')
             xml.field('0.35581450000000103', name='HalfHeight')
             xml.field('0.4184314473389928', name='Area')
             xml.field('2000-01-01T01:01:01Z', name='ContentDate')
-    print(xml)
+    return str(xml)
+
+def write_xmlfile(xml):
+	'''Takes xml string representation of row in xls and writes it to a file.'''
+	print xml
